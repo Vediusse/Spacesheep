7dd34d78791afd56adf69765832cf176658843ee
zeculesu <artiyjana@yandex.ru>
2024-06-01 10:08:12 +0300

    fix

--- src/jvmMain/java/io/github/zeculesu/itmo/prog5/GUI/Windows/ApplicationAbsract.java
+++ src/jvmMain/java/io/github/zeculesu/itmo/prog5/GUI/Windows/ApplicationAbsract.java
@@ -4,6 +4,6 @@ import io.github.zeculesu.itmo.prog5.GUI.UDPGui;
 import javafx.application.Application;
 
 public abstract class ApplicationAbsract extends Application {
-    UDPGui udpGui = new UDPGui("localhost",45000);
+    UDPGui udpGui = new UDPGui("localhost",45003);
 }
 
--- src/jvmMain/java/io/github/zeculesu/itmo/prog5/MainKt.kt
+++ src/jvmMain/java/io/github/zeculesu/itmo/prog5/MainKt.kt
@@ -8,7 +8,7 @@ import javafx.application.Application
 fun main(argv: Array<String>) {
     // Создайте объект UDPClient с указанными хостом и портом
     val host = "localhost"
-    val port = 45000
+    val port = 45003
     val udpClient = UDPGui(host, port)
 
     // Запустите приложение
