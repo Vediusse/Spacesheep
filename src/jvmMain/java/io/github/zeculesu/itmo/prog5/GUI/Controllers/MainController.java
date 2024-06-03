4860e4dca946a251ce27f094d0437b663226dd8b
zeculesu <artiyjana@yandex.ru>
2024-06-04 02:55:48 +0300

    fix button language

--- src/jvmMain/java/io/github/zeculesu/itmo/prog5/GUI/Controllers/MainController.java
+++ src/jvmMain/java/io/github/zeculesu/itmo/prog5/GUI/Controllers/MainController.java
@@ -116,6 +116,7 @@ public class MainController extends BaseController {
 
     @FXML
     public void initialize() {
+        highlightActiveLanguage(LanguageManager.getInstance().getActiveLanguageButtonId());
         StackPane.setAlignment(notificationsContainer, Pos.TOP_RIGHT);
         notificationsContainer.setPadding(new Insets(10)); // padding to move it away from the edge
 
@@ -179,7 +180,6 @@ public class MainController extends BaseController {
         ResourceManager.getInstance().localeProperty().addListener((observable, oldValue, newValue) -> {
             updateTexts();
         });
-
     }
 
 
@@ -208,6 +208,7 @@ public class MainController extends BaseController {
         this.bundle = ResourceManager.getInstance().getResourceBundle();
         this.updateTexts();
         highlightActiveLanguage(buttonId);
+        LanguageManager.getInstance().setActiveLanguageButtonId(buttonId);
     }
 
     public void updateTexts() {
